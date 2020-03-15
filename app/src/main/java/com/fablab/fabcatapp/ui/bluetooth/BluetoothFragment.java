13c90ed25d97f48689b490dc6a9300cb213fc4e4
Fedefreez <fedebosi04@gmail.com>
2020-03-15 17:38:04 +0100

    Added scrollView for positions fragment.

--- app/src/main/java/com/fablab/fabcatapp/ui/bluetooth/BluetoothFragment.java
+++ app/src/main/java/com/fablab/fabcatapp/ui/bluetooth/BluetoothFragment.java
@@ -6,7 +6,6 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
