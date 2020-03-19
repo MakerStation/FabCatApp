c0da9e6cba76c892bdbced3b061fbdd313d3f97a
Fede <fedebosi04@gmail.com>
2020-03-19 19:46:17 +0100

    Added home elements.

--- app/src/main/java/com/fablab/fabcatapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/fablab/fabcatapp/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.fablab.fabcatapp.ui.home;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,6 +23,18 @@ public class HomeFragment extends Fragment {
         FloatingActionButton fab = root.findViewById(R.id.fab);
         fab.setOnClickListener((view) -> BluetoothFragment.sendCustomCommand(view, getContext()));
 
+        Button visitWebsiteButton = root.findViewById(R.id.visitWebsiteButton);
+        visitWebsiteButton.setOnClickListener((v) -> goToUrl("http://makerstation.it"));
+
+        Button visitGithubRepositoryButton = root.findViewById(R.id.visitGithubRepositoryButton);
+        visitGithubRepositoryButton.setOnClickListener((v) -> goToUrl("https://github.com/Fedefreez/FabCatApp.git"));
+
         return root;
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
